a016ffdab8f7dd5fdf7a4e9fd0cdd4ef7b0c0092
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-07-07 13:10:58 -0500

    Add hilite function to prompt

--- jadsh/prompt.py
+++ jadsh/prompt.py
@@ -10,3 +10,15 @@ class Prompt():
         pwd = os.getcwd()
         sys.stdout.write(pwd + ":" + self.promptChar + ' ')
         sys.stdout.flush()
+
+	def hilite(self, string, status = False, bold = False):
+        attr = []
+        if status:
+            # green
+            attr.append('32')
+        else:
+            # red
+            attr.append('31')
+        if bold:
+            attr.append('1')
+        return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
