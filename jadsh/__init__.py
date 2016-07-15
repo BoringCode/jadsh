20246d36441be30cfccff58a560d14613883ffb8
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2016-07-15 15:18:33 -0500

    Remove old way of launching jadsh without installation

--- jadsh/__init__.py
+++ jadsh/__init__.py
@@ -1,9 +1,20 @@
+import sys
 from jadsh.shell import Shell
+import jadsh.constants as constants
 
-def main():
+def main(args = None):
 	"""
 	Launches a new instance of the shell
 	"""
+
+	if args is None:
+		args = sys.argv[1:]
+
+	# Parse arguments
+	if "--version" in args or "-v" in args:
+		print(constants.VERSION)
+		return
+
 	shell = Shell()
 
 if __name__ == "__main__":
